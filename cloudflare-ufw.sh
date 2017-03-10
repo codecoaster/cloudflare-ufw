990430186f15bac6f723adf456df5a9cbd8a6c2e
codecoaster <thecodecoaster@gmail.com>
2017-03-10 15:10:36 -0500

    updated script to only allow through ports 80 and 443

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -1,22 +1,9 @@
-#!/bin/sh
-
-DIR="$(dirname $(readlink -f $0))"
-cd $DIR
-wget https://www.cloudflare.com/ips-v4 -O ips-v4.tmp
-wget https://www.cloudflare.com/ips-v6 -O ips-v6.tmp
-mv ips-v4.tmp ips-v4
-mv ips-v6.tmp ips-v6
-
-for cfip in `cat ips-v4`; do ufw allow from $cfip; done
-for cfip in `cat ips-v6`; do ufw allow from $cfip; done
-
-ufw reload > /dev/null
-
-# OTHER EXAMPLE RULES
-# Examples to retrict to port 80
-#for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
-#for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 80 proto tcp; done
-
-# Examples to restrict to port 443
-#for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 443 proto tcp; done
-#for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 443 proto tcp; done
+#!/bin/bash
+wget https://www.cloudflare.com/ips-v4 
+wget https://www.cloudflare.com/ips-v6 
+for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
+for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 80 proto tcp; done
+for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 443 proto tcp; done
+for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 443 proto tcp; done
+ufw reload
+ufw status
