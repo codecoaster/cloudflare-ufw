42e955915dc8489672cad50a9da4417d93437125
codecoaster <thecodecoaster@gmail.com>
2017-03-10 15:12:10 -0500

    updated script to only allow through ports 80 and 443

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 wget https://www.cloudflare.com/ips-v4 
 wget https://www.cloudflare.com/ips-v6 
-for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
-for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 80 proto tcp; done
-for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 443 proto tcp; done
-for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 443 proto tcp; done
+    for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
+    for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 80 proto tcp; done
+    for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 443 proto tcp; done
+    for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 443 proto tcp; done
 ufw reload
 ufw status
